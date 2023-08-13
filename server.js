0d3043ba62968d5ef8f06ecdbd817fc646716e42
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-08-13 15:52:14 -0700

    testing out...

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: 'https://mari-miyazaki.netlify.app',
+    methods: ['GET', 'POST'],
+  }));
 app.use(express.json());
 app.use('/', router);
 app.listen(5000, () => console.log('Server Running!'));
