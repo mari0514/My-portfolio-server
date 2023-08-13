4d17de8d65d5f63dfd2eecaeb87b000e3eab06dc
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-08-13 16:28:48 -0700

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors({
   }));
 app.use(express.json());
 app.use('/', router);
-app.listen('5000', () => console.log('Server Running!'));
+app.listen(5000, () => console.log('Server Running!'));
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
